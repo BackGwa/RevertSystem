7ef3d954ea3bbdd308e64d4596e709e591d16fd4
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-10-13 17:57:57 +0900

    SubArduino

--- RevertSystemSecond/RevertSystemSecond.ino
+++ RevertSystemSecond/RevertSystemSecond.ino
@@ -1,6 +1,9 @@
 
 #include <MFRC522.h>
+#include <Servo.h>
+#include <SPI.h>
 
+#define NORMAL_ANGLE 0
 #define SS_PIN 10
 #define RST_PIN 9
 #define A1_MOTOR 5
@@ -8,54 +11,83 @@
 #define BOARD_RATE 9600
 
 MFRC522 RFID(SS_PIN, RST_PIN);
+Servo SERVO;
 
-String RFID_VALUE = "00 00 00 00";
+String RFID_VALUE = "32 6D 56 D3";
+int RFID_RETURN = 0;
+int looptime = 0;
 
-char isRFID();
+int isRFID();
 void TurnMotor(uint8_t SPEED = 0);
 void Send(int DATA);
+void ServoUse(int SERVO_PIN, int ANGLE = NORMAL_ANGLE, int DELAY = 0);
+bool loopturn = false;
 
 void setup(){
   Serial.begin(BOARD_RATE);
   SPI.begin();
   RFID.PCD_Init();
+
+  ServoUse(3, 70);
+  TurnMotor(0);
 }
 
 
 void loop(){
+
+  if(!loopturn){
+    RFID_RETURN = isRFID();
+  }
+
+  if(RFID_RETURN == 1 && !loopturn){
+    delay(500);
+    ServoUse(3, 180);
+    delay(1250);
+    ServoUse(3, 70);
+    TurnMotor(175);
+    loopturn = true;
+    Serial.println("LOOPING...");
+  }
+
+  else if(loopturn){
+    if(looptime >= 4000){
+      TurnMotor();
+      looptime = 0;
+      loopturn = false;
+      return;
+      }
+      delay(1);
+      looptime++;
+  }
 }
 
-
-char isRFID(){
+int isRFID(){
   String content= "";
   byte letter;
 
-  if (!rfid.PICC_IsNewCardPresent()){
-    return 'N';
+  if ( ! RFID.PICC_IsNewCardPresent()){
+    return 0;
   }
   
-  if (!rfid.PICC_ReadCardSerial()){
-    return 'N';
+  if ( ! RFID.PICC_ReadCardSerial()){
+    return 0;
   }
 
   Serial.print("RFID VALUE >> ");
 
-  for (byte i = 0; i < rfid.uid.size; i++){
-     Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(rfid.uid.uidByte[i], HEX);
-     content.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(rfid.uid.uidByte[i], HEX));
+  for (byte i = 0; i < RFID.uid.size; i++){
+     Serial.print(RFID.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(RFID.uid.uidByte[i], HEX);
+     content.concat(String(RFID.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(RFID.uid.uidByte[i], HEX));
   }
 
-  content.toUpperCase();
+  Serial.println();
 
-  if (content.substring(1) == RFID_VALUE){
-    return 'T'; 
-  }
-  
-  else{ 
-    return 'F';
-  }
+  //content.toUpperCase();
+  //content.substring(1) == RFID_VALUE
+
+  return 1;
 }
 
 void TurnMotor(uint8_t SPEED = 0){
@@ -65,4 +97,12 @@ void TurnMotor(uint8_t SPEED = 0){
 
 void Send(int DATA){
   Serial.print(DATA);
+}
+
+void ServoUse(int SERVO_PIN, int ANGLE = NORMAL_ANGLE, int DELAY = 0)
+{
+    SERVO.attach(SERVO_PIN);
+    SERVO.write(ANGLE);
+
+    delay(DELAY);
 }
